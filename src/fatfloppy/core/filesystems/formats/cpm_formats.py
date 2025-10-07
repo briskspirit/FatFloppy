fe00c41b90c5bb538b237c90f9da9fcfd23179b4
briskspirit <bs@privacy.im>
2025-10-07 12:23:34 -0400

    Fix creation of MITS_DSK format

--- src/fatfloppy/core/filesystems/formats/cpm_formats.py
+++ src/fatfloppy/core/filesystems/formats/cpm_formats.py
@@ -232,7 +232,7 @@ def _create_mits_altair_variants() -> list[tuple[str, PhysicalFormat]]:
         rpm=360,
         heads_inverted=False,
         bytes_per_sector=128,
-        image_in_sector_id_order=True,
+        image_in_sector_id_order=False,
         track_formats=[
             TrackFormat(
                 track_start=0,
